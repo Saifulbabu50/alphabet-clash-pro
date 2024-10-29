37ddd90e0f5cd0e36e95c6ec7d6942360dc6a032
Saifulbabu50 <srmdsaifulsaif@gmail.com>
2024-10-29 23:16:11 +0600

    fast function dieclerasion

--- script/script.js
+++ script/script.js
@@ -1,3 +1,16 @@
+// function play() {
+//     // step-1: hide the home screen.to hied the screen add the class hidden to the home section
+//     const homeSection = document.getElementById('home-screen');
+//     homeSection.classList.add('hidden');
+//     // console.log(homeSection.classList)
+
+//     // show the playground
+//     const playGround = document.getElementById('play-ground');
+//     playGround.classList.remove('hidden');
+//     // console.log(playGround.classList)
+// }
+
 function play() {
-    console.log('mama khala hobe')
+    hideElementById('home-screen')
+    showElementById('play-ground')
 }
